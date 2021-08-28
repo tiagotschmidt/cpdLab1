0149a050163c10e6cf95aaaa9e1efd0b540c7ed7
Tiago Torres Schmidt <tiago.schmidt@inf.ufrgs.br>
2021-08-28 16:27:12 -0300

    Funções da primeira parte do laboratório comentadas.

--- shell.c
+++ shell.c
@@ -7,26 +7,25 @@
 #define DEFAULT_INPUT "entrada1.txt"
 #define DEFAULT_OUTPUT "saida.txt"
 
-void prtArray(int array[], int vectorLength, FILE* output){
+void prtArray(int array[], int vectorLength, FILE* output){//Função prtArray. Printa a Array informada dentro do arquivo apontado por *output.
   int i;
-  for(i=0;i<vectorLength;i++){
+  for(i=0;i<vectorLength;i++){//Iteração básica pelo tamanho do Array.
     fprintf(output,"%d ",array[i]);
   }
 }
-void prtArrayCLI(int array[], int vectorLength){
+void prtArrayCLI(int array[], int vectorLength){//Função prtArrayCLI. Printa a Array informada dentro do console. Utilizado em DEV.
   int i;
-  for(i=0;i<vectorLength;i++){
+  for(i=0;i<vectorLength;i++){//Iteração básica pelo tamanho do Array.
     printf("%d ",array[i]);
   }
 }
 
-void shellSortBASE2(int array[], int vectorLength, FILE* output){
+void shellSortBASE2(int array[], int vectorLength, FILE* output){//Implementação do algoritmo shellSort, com base nos INCREMENTOS de base 2.
   int i,j,k,tmp;
 
-  for(i=vectorLength/2;i>0;i=i/2){//Incremento.
-    for(j=i;j<vectorLength;j++){//Percorre cada grupo.
-      for(k=j-i;k>=0;k=k-1){//
-        //printf("I:%d\tJ:%d\tK:%d\n",i,j,k);
+  for(i=vectorLength/2;i>0;i=i/2){//Incremento atual.
+    for(j=i;j<vectorLength;j++){//Percorre cada item de cada grupo formado pelo incremento.
+      for(k=j-i;k>=0;k=k-1){//Percorre todos os grupos formados pelo incremento.
         if(array[k+i]>=array[k]){//Se o elemento já estiver ordenado, para.
           break;
         }else{//Se não:Executa troca,
@@ -41,18 +40,17 @@ void shellSortBASE2(int array[], int vectorLength, FILE* output){
   }
 }
 
-void shellSortKNUTH(int array[], int vectorLength, FILE* output){
+void shellSortKNUTH(int array[], int vectorLength, FILE* output){//Implementação do algoritmo shellSort, com base nos INCREMENTOS de KNUTH.
   int i,j,k,tmp,initI;
 
   initI = 1;
-  while((initI * 3 + 1) < vectorLength){
+  while((initI * 3 + 1) < vectorLength){//Definição do primeiro INCREMENTO utilizado, segundo a base de KNUTH.
     initI = initI * 3 + 1;
   }
 
-  for(i=initI;i>0;i=i/3){//Incremento.
-    for(j=i;j<vectorLength;j++){//Percorre cada grupo.
-      for(k=j-i;k>=0;k=k-1){//
-        //printf("I:%d\tJ:%d\tK:%d\n",i,j,k);
+  for(i=initI;i>0;i=i/3){//Incremento atual.
+    for(j=i;j<vectorLength;j++){//Percorre cada item de cada grupo formado pelo incremento.
+      for(k=j-i;k>=0;k=k-1){//Percorre todos os grupos formados pelo incremento.
         if(array[k+i]>=array[k]){//Se o elemento já estiver ordenado, para.
           break;
         }else{//Se não:Executa troca,
@@ -67,21 +65,20 @@ void shellSortKNUTH(int array[], int vectorLength, FILE* output){
   }
 }
 
-void shellSortCIURA(int array[], int vectorLength, FILE* output){
+void shellSortCIURA(int array[], int vectorLength, FILE* output){//Implementação do algoritmo shellSort, com base nos INCREMENTOS de CIURA.
   int i,j,k,tmp,initI;
-  int ciuraBASE[] = { 1,4,10,23,57,132,301,701,1577,3548,7983,17961,40412,90927,204585,460316};
+  int ciuraBASE[] = { 1,4,10,23,57,132,301,701,1577,3548,7983,17961,40412,90927,204585,460316};//Base de CIURA.
   int currentCiura;
 
   initI=0;
-  while(vectorLength > ciuraBASE[initI+1]){
+  while(vectorLength > ciuraBASE[initI+1]){//Define o incremento inicial, baseado na base de CIURA.
     initI++;
   }
 
-  while(initI>=0){//Incremento.
-    i = ciuraBASE[initI];
-    for(j=i;j<vectorLength;j++){//Percorre cada grupo.
-      for(k=j-i;k>=0;k=k-1){//
-        //printf("I:%d\tJ:%d\tK:%d\n",i,j,k);
+  while(initI>=0){//Índice de incremento, roda até chegar ao 0.
+    i = ciuraBASE[initI];//Incremento atual.
+    for(j=i;j<vectorLength;j++){//Percorre cada item de cada grupo formado pelo incremento.
+      for(k=j-i;k>=0;k=k-1){//Percorre todos os grupos formados pelo incremento.
         if(array[k+i]>=array[k]){//Se o elemento já estiver ordenado, para.
           break;
         }else{//Se não:Executa troca,
@@ -93,72 +90,42 @@ void shellSortCIURA(int array[], int vectorLength, FILE* output){
     }
     prtArray(array,vectorLength,output);
     fprintf(output,"INCR=%d\n",i);
-    initI--;
+    initI--;//Reduz o índice de Incremento.
   }
 }
 
-void shellSort(int array[], int vectorLength, FILE* output){
-  int i,j,k,tmp;
-
-  for(i=vectorLength/2;i>0;i=i/2){//Incremento.
-    for(j=i;j<vectorLength;j++){//Percorre cada grupo.
-      for(k=j-i;k>=0;k=k-1){//
-        //printf("I:%d\tJ:%d\tK:%d\n",i,j,k);
-        if(array[k+i]>=array[k]){//Se o elemento já estiver ordenado, para.
-          break;
-        }else{//Se não:Executa troca,
-          tmp = array[k];
-          array[k] = array[k+i];
-          array[k+i] = tmp;
-        }
-      }
-    }
-    prtArray(array,vectorLength,output);
-    fprintf(output,"INCR=%d\n",i);
-  }
-}
-
-int main(){
-  int buffer[MAX_ITEMS];
-  int bufferClone[MAX_ITEMS];
-  int nItems;
-  int i,j,k;
-  fpos_t position;
-  FILE* input;
+int main(){//Função main. Executa os testes requisitados.
+  int buffer[MAX_ITEMS];//Variáveis utilizadas nos testes.
+  int bufferClone[MAX_ITEMS];//Buffer e BufferClone são os espaços de leitura dos vetores do .txt
+  int nItems;//Quantifica quantos itens existem no array.
+  int i,j,k;//Índices de uso geral.
+  FILE* input;//Ponteiros para os arquivos.
   FILE* output;
   char seqName[3][MAX_CHAR] = {"SHELL\n","KNUTH\n","CIURA\n"};
-  char cName[MAX_CHAR];
+  char cName[MAX_CHAR];//Sistema para impressão da sequência atual.
 
-  if((input = fopen(DEFAULT_INPUT,"r")) == NULL){
-    printf("Erro ao abrir arquivo %s \n",DEFAULT_INPUT);
-  }else{
-    fgetpos(input, &position);
-    if((output = fopen(DEFAULT_OUTPUT,"w")) == NULL){
-      printf("Erro ao abrir arquivo %s \n",DEFAULT_OUTPUT);
-    }else{
-      while(feof(input)==0){
-        fscanf(input,"%d ",&nItems);
-        for(i=0;i<nItems;i++){
-          fscanf(input,"%d ",&buffer[i]);
+  if((input = fopen(DEFAULT_INPUT,"r")) == NULL){//Tenta abrir o arquivo de entrada,
+    printf("Erro ao abrir arquivo %s \n",DEFAULT_INPUT);//Caso de falha.
+  }else{//Caso sucesso, (default):
+    if((output = fopen(DEFAULT_OUTPUT,"w")) == NULL){//Tenta abrir o arquivo de saída.
+      printf("Erro ao abrir arquivo %s \n",DEFAULT_OUTPUT);//Caso de falha.
+    }else{//Caso sucesso, (default):
+      while(feof(input)==0){//Enquanto não encontra o fim do arquivo de entrada.
+        fscanf(input,"%d ",&nItems);//Resgata o número de itens no array.
+        for(i=0;i<nItems;i++){//Para cada item no array, armazena um número lido.
+          fscanf(input,"%d ",&buffer[i]);//Executa a leitura do .txt de entrada.
         }
-        /*printf("nItens:%d\n",nItems);/*
-        puts("\nVetor:");
-        prtArrayCLI(buffer,nItems);
-        puts("\n"); */
 
-
-        //printf("J:%d K:%d\n",j,k);
-
-        for(j=0;j<3;j++){
-          for(k=0; k < MAX_ITEMS; k++) {
+        for(j=0;j<3;j++){//Para cada array lido do .txt, executa três variações do shell....
+          for(k=0; k < MAX_ITEMS; k++) {//Copia o buffer para bufferClone.
             bufferClone[k] = buffer[k];
           }
-          prtArray(buffer,nItems,output);
+          prtArray(buffer,nItems,output);//Print incial de cada ordenamento...
           fprintf(output,"SEQ=");
           fputs(&seqName[j-1][MAX_CHAR],output);
-          switch(j){
+          switch(j){//Varia entres os tipos de shell.
             case 0:
-              shellSort(bufferClone, nItems, output);
+              shellSortBASE2(bufferClone, nItems, output);
               break;
             case 1:
               shellSortKNUTH(bufferClone, nItems, output);
@@ -172,7 +139,7 @@ int main(){
     }
   }
 
-  fclose(output);
+  fclose(output);//Fecha os ponteiros de leitura.
   fclose(input);
   return 0;
 }
